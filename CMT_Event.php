0e299a9c66d37459c57edfa5cc068b50fa86e5f4
Lyuei <49364719+Lyuei@users.noreply.github.com>
2022-05-11 20:19:29 +0800

    Create CMT_Event.php

---
+++ CMT_Event.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include 'nav.php'; 
+?>
+
+<head>
+    <title>Events</title>
+</head>
+
+<body>
+    <br>
+    <center>
+        <h1 class="display-5 fw-bold">Upcoming Events</h1>
+    </center>
+    <div class="container my-5">
+        <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 flex-lg-row-reverse align-items-center rounded-3 border shadow-lg">
+            <div class="col-10 col-sm-8 col-lg-5">
+                <img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/cd62f3f748e1eefa43297ddb2f0e43563f3ce3de0002dcdc13d146122f46eaec.jpg"
+                    class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="400" height="400" loading="lazy">
+                <br>
+                <br>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus velit
+                    et quos explicabo voluptates ullam iste quas eligendi recusandae tempora mollitia modi magni, amet,
+                    totam laborum deserunt itaque hic.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5">
+        <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 flex-lg-row-reverse align-items-center rounded-3 border shadow-lg">
+            <div class="col-10 col-sm-8 col-lg-5">
+                <img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/cd62f3f748e1eefa43297ddb2f0e43563f3ce3de0002dcdc13d146122f46eaec.jpg"
+                    class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="400" height="400" loading="lazy">
+                <br>
+                <br>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus velit
+                    et quos explicabo voluptates ullam iste quas eligendi recusandae tempora mollitia modi magni, amet,
+                    totam laborum deserunt itaque hic.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
