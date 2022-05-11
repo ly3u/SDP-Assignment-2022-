22060ff6cba7c170cf3bb2d4154f249479e76219
Lyuei <49364719+Lyuei@users.noreply.github.com>
2022-05-12 00:52:09 +0800

    Update CMT_Event.php

--- CMT_Event.php
+++ CMT_Event.php
@@ -10,10 +10,17 @@
 </head>
 
 <body>
-    <br>
-    <center>
-        <h1 class="display-5 fw-bold">Upcoming Events</h1>
-    </center>
+    <div class="container my-5">
+        <div class="d-flex p-5 mb-4 rounded-3">
+            <div class="container-fluid py-5 ">
+                <center>
+                    <h1 class="display-5 fw-bold">Upcoming Events</h1>
+                </center>
+            </div>
+            <button class="col-2 btn btn-primary btn-lg" type="button" style="height:100px; margin-top:40px;">Add
+                Event</button>
+        </div>
+    </div>
     <div class="container my-5">
         <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 flex-lg-row-reverse align-items-center rounded-3 border shadow-lg">
             <div class="col-10 col-sm-8 col-lg-5">
@@ -28,8 +35,8 @@
                     et quos explicabo voluptates ullam iste quas eligendi recusandae tempora mollitia modi magni, amet,
                     totam laborum deserunt itaque hic.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                    <button type="button" class="btn btn-primary btn-lg px-8 me-md-2">Submit Report</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Edit</button>
                 </div>
             </div>
         </div>
@@ -48,8 +55,37 @@
                     et quos explicabo voluptates ullam iste quas eligendi recusandae tempora mollitia modi magni, amet,
                     totam laborum deserunt itaque hic.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                    <button type="button" class="btn btn-primary btn-lg px-8 me-md-2">Submit Report</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Edit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container my-5">
+        <div class="d-flex p-5 mb-4 rounded-3">
+            <div class="container-fluid py-5 ">
+                <center>
+                    <h1 class="display-5 fw-bold">Past Events</h1>
+                </center>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5">
+        <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 flex-lg-row-reverse align-items-center rounded-3 border shadow-lg">
+            <div class="col-10 col-sm-8 col-lg-5">
+                <img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/cd62f3f748e1eefa43297ddb2f0e43563f3ce3de0002dcdc13d146122f46eaec.jpg"
+                    class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="400" height="400" loading="lazy">
+                <br>
+                <br>
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus velit
+                    et quos explicabo voluptates ullam iste quas eligendi recusandae tempora mollitia modi magni, amet,
+                    totam laborum deserunt itaque hic.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-8 me-md-2">View Feedbacks</button>
                 </div>
             </div>
         </div>
