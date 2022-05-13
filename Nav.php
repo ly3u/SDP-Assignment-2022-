8a5031ad2848c8a20110308b699adc1691e84907
BenOngCW <103731714+BenOngCW@users.noreply.github.com>
2022-05-13 16:08:05 +0800

    Update Nav.php

--- Nav.php
+++ Nav.php
@@ -8,7 +8,11 @@
     <title>APU Club and Society</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="maincss.css">
+=======
+
+>>>>>>> Stashed changes
 </head>
 
 <body>
