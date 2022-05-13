de4b3b1b5a1e4c6a9017e446c6b7d8bbb15c4317
Lyuei <49364719+Lyuei@users.noreply.github.com>
2022-05-13 15:05:58 +0800

    Update S_ClubInfo.php

--- S_ClubInfo.php
+++ S_ClubInfo.php
@@ -6,8 +6,7 @@
 ?>
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Club Information</title>
 </head>
 
 <body>
@@ -21,17 +20,15 @@
                     your liking.</p>
                 <button class="btn btn-primary btn-lg" type="button">Example button</button>
             </div>
-
             <img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/cd62f3f748e1eefa43297ddb2f0e43563f3ce3de0002dcdc13d146122f46eaec.jpg"
                 alt="" style="width:250px; height:250px;">
         </div>
-
 
         <div class="bd-example">
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
+                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="425"
                             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide"
                             preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>Placeholder</title>
@@ -42,7 +39,7 @@
 
                     </div>
                     <div class="carousel-item">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
+                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="425"
                             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide"
                             preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>Placeholder</title>
@@ -53,7 +50,7 @@
 
                     </div>
                     <div class="carousel-item">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
+                        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="425"
                             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide"
                             preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>Placeholder</title>
@@ -77,7 +74,7 @@
             </div>
         </div>
         <br>
-        <h1>Events</h1>
+        <h1 class="display-5 fw-bold">Events</h1>
         <div class="album py-5 bg-light">
             <div class="container">
 
