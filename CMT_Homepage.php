139054101bda2d26f1f1e7ab54f5fa952ffbdf22
Lyuei <49364719+Lyuei@users.noreply.github.com>
2022-05-30 03:12:58 +0800

    sss

--- CMT_Homepage.php
+++ CMT_Homepage.php
@@ -30,99 +30,117 @@
 <head>
     <title>Club Information </title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="popup.css">
     <script>
     if (window.history.replaceState) {
         window.history.replaceState(null, null, window.location.href);
+    }
+
+    function openForm() {
+        document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+        document.getElementById("myForm").style.display = "none";
     }
     </script>
 </head>
 
 <body>
     <?php
-        include 'nav_club.php'; 
+    if(isset($email)){
+        require_once ('nav_login.php');
+    }else{
+        require_once ('Nav.php');
+    }
     ?>
-    <div class="container py-4">
-        <div class="d-flex p-5 mb-4 bg-light rounded-3" style=" border-radius: 25px;">
+    <div class="container py-4" style="overflow-anchor: none;">
+        <div class="d-flex p-4 mb-4 bg-light rounded-3" style=" border-radius: 25px;">
             <div class="container-fluid py-5">
                 <h1 class="display-5 fw-bold"><?php echo $row['C_Name']; $CID=$row['C_ID'];?></h1>
                 <form method="POST">
-                    <textarea class="col-md-11 fs-4" type="text" name="description" rows="7"
-                        class="form-control"><?php echo $description; ?></textarea>
+                    <textarea class="col-md-11 fs-4" type="text" name="description"
+                        rows="7"><?php echo $description; ?></textarea>
                     <br>
-                    <button class="btn btn-secondary btn-lg" name="Save">Save</button>
+                    <button class="btn btn-secondary btn-lg" name="dUpdate">Update</button>
                 </form>
             </div>
             <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['C_Logo']); ?>" alt=""
                 style="width:250px; height:250px; margin-top: 25px;">
         </div><br>
-        <div class="bd-example">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner" style=" border-radius: 25px;">
-                        <div class="carousel-item active">
-                            <a href="google.com">
-                                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
-                                    height="425" role="img" aria-label="Placeholder: First slide"
-                                    preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <rect width="100%" height="100%" fill="#1982c4" />
-                                    <text class="display-2 fw-bold" x="22.5%" y="50%" dy=".3em"
-                                        style="text-decoration: none;">Edit
-                                        Annoucement
-                                    </text>
-                                </svg>
-                        </div></a>
-                        <?php
-                        while ($row1 = mysqli_fetch_array($result1)) { ?>
-                        <div class="carousel-item">
-                            <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800"
-                                height="425">
-                                <rect width="100%" height="100%" fill="#bbd6ed" />
-                                <text class="display-7" x="15%" y="20%" fill="#111" dy=".3em">Date
-                                    Posted: <?php echo $row1["Date_Post"] ?>
-                                </text>
-                                <text class="display-6" x="15%" y="35%" fill="#111"
-                                    dy=".3em"><?php echo $row1["Announcement"] ?>
-                                </text>
-                                <a href=" CMT_EventEdit.php?Eid=<?php echo $row2['E_ID'] ?>" style=" color:black;"><text
-                                        type="button" class="btn btn-sm btn-outline-secondary"></text>
-                                </a>
-                            </svg>
-                        </div>
-                        <?php } ?>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
+        <div class="d-flex p-5 mb-4 bg-light rounded-3" style=" border-radius: 25px;">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Announcement</h1>
+                <?php while ($row1 = mysqli_fetch_array($result1)) { ?>
+                <h4 class="display-9 fw-bold">Date Posted: <?php echo $row1["Date_Post"]?></h4>
+                <form method="POST">
+                    <textarea class="col-md-11 fs-4" type="text" name="a<?php echo $row1["A_ID"]?>" rows="7"
+                        class="form-control"><?php echo $row1["Announcement"]; ?></textarea>
+                    <br>
+                    <?php aUpdate($row1["A_ID"]); ?>
+                    <button class="btn btn-danger btn-lg" name="aDelete<?php echo $row1["A_ID"]?>">Delete</button>
+                    <br>
+                    <br>
+                </form>
+                <?php } ?>
             </div>
         </div>
-    </div>
-    <div class="form-popup" id="myForm">
-        <form action="" method="post" class="form-container">
-            <h1>Feedback</h1>
-            <input type="hidden" id='myText' name='id'>
-            <textarea type="text" placeholder="Enter Feedback" rows="4" name="feedbackText" required></textarea>
-
-            <button type="submit" class="btn" name="submit">Confirm</button>
-            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-        </form>
+        <br>
+        <div class="d-flex p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Club Committees</h1>
+                <form method="POST">
+                    <h3 class="display-9 fw-bold">Club Advisor</h3>
+                    <input class="col-md-7 fs-4" type="text" name="cAdvisor"
+                        value="<?php echo $row['C_Advicer']?>"></input>
+                    <br>
+                    <br>
+                    <h3 class="display-9 fw-bold">President</h3>
+                    <input class="col-md-7 fs-4" type="text" name="cPresident"
+                        value="<?php echo $row['P_Name']?>"></input>
+                    <br>
+                    <br>
+                    <h3 class="display-9 fw-bold">Vice President</h3>
+                    <input class=" col-md-7 fs-4" type="text" name="cVPresident"
+                        value="<?php echo $row['VP_Name']?>"></input>
+                    <br>
+                    <br>
+                    <button class=" btn btn-info btn-lg" name="cmtUpdate">Update</button>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 
 </html>
 
 <?php 
-    if(isset($_POST['Save'])) {
+    if(isset($_POST['dUpdate'])) {
         $description = $_POST['description'];
         $sql2 ="UPDATE club SET C_Description = '$description' WHERE C_ID='$CID'";
         $result2 = mysqli_query($con, $sql2);  
+        echo  "<script>window.location.reload();</script>"; 
+    }
+    function aUpdate($A_ID) {
+        echo '<button class="btn btn-primary btn-lg" name="'.$A_ID.'">Update</button>';
+        if (isset($_POST[$A_ID])) {
+            $announcement = $_POST['a'.$A_ID];
+            $aSQL ="UPDATE announcement SET Announcement = '$announcement' WHERE A_ID='$A_ID'";
+            $_SESSION['aID'] = $aSQL; 
+            echo  '<script>window.location.reload();</script>'; 
+        }
+    }
+    
+    if (isset($_SESSION['aID'])) {
+        $result2 = mysqli_query($con, $_SESSION['aID']);        
+    }
+
+    if(isset($_POST['cmtUpdate'])) {
+        $cAdvisor = $_POST["cAdvisor"];
+        $cPresident = $_POST["cPresident"];
+        $cVPresident = $_POST["cVPresident"];
+        $cmtSQL ="UPDATE club SET C_Advicer = '$cAdvisor', P_Name = '$cPresident', VP_Name = '$cVPresident' WHERE C_ID='$CID'";
+        $cmtResult = mysqli_query($con, $cmtSQL);  
         echo  "<script>window.location.reload();</script>"; 
     }
 ?>
